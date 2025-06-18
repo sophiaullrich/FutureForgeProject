0f99d9433ba0581fe1937ed178cace00fa0fc511
diya <diya_topiwala@outlook.com>
2025-06-18 22:26:26 +1200

    Nav Bar Layout Complete
    - Need to add link to settings page and fix settings icon

--- frontend/src/NavigationBar.jsx
+++ frontend/src/NavigationBar.jsx
@@ -2,6 +2,7 @@
 import React, {useState, useEffect, useRef} from 'react';
 import { Link, useLocation } from 'react-router-dom'; // useLocation to check active link
 import './NavigationBar.css'; 
+import logo from './assets/gobearlogo.png';
 
 function NavigationBar() {
   const location = useLocation(); // Hook to get current path
@@ -33,7 +34,7 @@ function NavigationBar() {
     <div className="navbar-container">
       <div className="navbar-header">
         <div className="navbar-bg">
-          <img src="frontend/assets/gobearlogo.png" alt="Go Bear Logo" className="logo" /> 
+          <img src={logo} alt="Go Bear Logo" className="logo" /> 
         </div>
       </div>
 
@@ -41,7 +42,8 @@ function NavigationBar() {
         <ul>
           {['Dashboard', 'Teams', 'Tasks', 'Rewards', 'Chat'].map((label, index) => {
           const path = `/${label.toLowerCase()}`;
-
+          const isActive = location.pathname === path;
+          
           return (
             <li key={index} ref={el => itemRefs.current[index] = el}>
               <Link
@@ -50,18 +52,18 @@ function NavigationBar() {
                 onClick={() => handleClick(index)}
               >
                 {label}
+                {isActive && <span ref={lineRef} className='underline-line'></span>}
               </Link>
             </li>
           );
         })}
         </ul>
-        <span ref={lineRef} className="underline-line"></span>
+        {/* <span ref={lineRef} className="underline-line"></span> */}
       </nav>
 
       <div className="navbar-footer">
         {/* Placeholder for the settings icon */}
         <span role="img" aria-label="settings">⚙️</span>
-        {/* You'll add your actual icon here */}
       </div>
     </div>
   );
