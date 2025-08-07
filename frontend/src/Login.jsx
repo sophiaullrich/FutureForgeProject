b9532a367d64aba8e1b746abf451cc5c37622bdd
MarcosFig-dev <marcos.amfig@gmail.com>
2025-08-07 16:38:59 +1200

    Updated Login and Signup
    Login now works with firebase
    Signup now works with firebase
    Added logout functionality
    Added reset password functionality

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,54 +1,90 @@
 import React from "react";
+import firebaseApp from "./Firebase.js";
 import "./Login.css";
 import logo from "./assets/gobearlogo.png";
 import googleicon from "./assets/Google Icon.png";
 import githubicon from "./assets/GitHub Icon.png";
 import linkedinicon from "./assets/LinkedIn Icon.png";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+} from "firebase/auth";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const Login = () => (
-  <div id="body">
-    <div id="title-cont">
-      <img src={logo} alt="GoBear Logo" />
-    </div>
+function Login() {
+  const auth = getAuth(firebaseApp);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
-    <div id="form-cont">
-      <h1>Login</h1>
-      <form>
-        <label for="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="Email"
-          required
-        />
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (error) {
+      let message = error.message;
+
+      if (error.code === "auth/invalid-credential") {
+        message = `Incorrect Password or Email`;
+      }
+      alert(message);
+    }
+  };
+
+  return (
+    <div id="body">
+      <div id="title-cont">
+        <img src={logo} alt="GoBear Logo" />
+      </div>
+
+      <div id="form-cont">
+        <h1>Login</h1>
+        <form onSubmit={handleLogin}>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            required
+          />
+          <br />
+          <label htmlFor="pword">Password:</label>
+          <input
+            type="password"
+            id="pword"
+            name="pword"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <br />
+          <input type="submit" value="Login" />
+          {error && <p className="error">{error}</p>}
+        </form>
+
+        <a href="./Resetpass">Forgot Password?</a>
+
+        <p>or continue with</p>
+        <img src={googleicon} alt="Continue with Google" />
+        <img src={githubicon} alt="Continue with GitHub" />
+        <img src={linkedinicon} alt="Continue with LinkedIn" />
         <br />
-        <label for="pword">Password:</label>
-        <input
-          type="password"
-          id="pword"
-          name="pword"
-          placeholder="Password"
-          required
-        />
-        <br />
-        <input type="submit" value="Sign In" />
-      </form>
 
-      <a href="./Resetpass">Forgot Password?</a>
-
-      <p>or continue with</p>
-      <img src={googleicon} alt="Continue with Google" />
-      <img src={githubicon} alt="Continue with GitHub" />
-      <img src={linkedinicon} alt="Continue with LinkedIn" />
-      <br />
-
-      <p>Don't have an account?</p>
-      <a href="./Signup" class="button-link">
-        Sign Up
-      </a>
+        <p>Don't have an account?</p>
+        <a href="./Signup" className="button-link">
+          Sign Up
+        </a>
+      </div>
     </div>
-  </div>
-);
+  );
+}
 
 export default Login;
--- frontend/src/NavigationBar.jsx
+++ frontend/src/NavigationBar.jsx
@@ -1,100 +1,149 @@
+import React, { useState, useEffect, useRef } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom"; // useLocation to check active link
+import "./NavigationBar.css";
+import logo from "./assets/gobearlogo.png";
+import { IoSettingsOutline, IoSettings } from "react-icons/io5";
+import firebaseApp from "./Firebase.js";
+import { getAuth, signOut } from "firebase/auth";
 
-import React, {useState, useEffect, useRef} from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom'; // useLocation to check active link
-import './NavigationBar.css'; 
-import logo from './assets/gobearlogo.png';
-import { IoSettingsOutline, IoSettings } from 'react-icons/io5';
 
 function NavigationBar() {
-  const location = useLocation(); // Hook to get current path
-  
-  // for underline
-  const [activeIndex, setActiveIndex] = useState(0);
-  const itemRefs = useRef([]);
-  const lineRef = useRef(null);
+ const location = useLocation(); // Hook to get current path
 
-  // for settings button
-  const [settingsOpen, setSettingsOpen] = useState(false);
-  const [hovered, setHovered] = useState(false);
-  const navigate = useNavigate();
-  const previousPathRef = useRef(null);
 
-  useEffect(() => {
-    const activeEl = itemRefs.current[activeIndex];
-    if (activeEl && lineRef.current) {
-      const link = activeEl.querySelector('a');
+ // for underline
+ const [activeIndex, setActiveIndex] = useState(0);
+ const itemRefs = useRef([]);
+ const lineRef = useRef(null);
 
-      const { offsetTop, offsetHeight, offsetLeft, offsetWidth } = link;
 
-      lineRef.current.style.top = `${offsetTop}px`; // align with top of link
-      lineRef.current.style.left = `0px`; // stay flush left
-      lineRef.current.style.width = `100%`; // full width of parent
-      lineRef.current.style.height = `3px`; // horizontal line like 
-    }
-  }, [activeIndex]);
+ // for settings button
+ const [settingsOpen, setSettingsOpen] = useState(false);
+ const [hovered, setHovered] = useState(false);
+ const navigate = useNavigate();
+ const previousPathRef = useRef(null);
 
-  const handleClick = (index) => {
-    setActiveIndex(index);
-  };
 
-  return (
-    <div className="navbar-container">
-      <div className="navbar-header">
-        <div className="navbar-bg" 
-          onClick={()=> {
-            setActiveIndex(0);
-            navigate('/dashboard');
-          }} style={{cursor: 'pointer'}}>
-          <img src={logo} alt="Go Bear Logo" className="logo" /> 
-        </div>
-      </div>
+ useEffect(() => {
+   const activeEl = itemRefs.current[activeIndex];
+   if (activeEl && lineRef.current) {
+     const link = activeEl.querySelector("a");
 
-      <nav className="navbar-nav">
-        <ul>
-          {['Dashboard', 'Teams', 'Tasks', 'Rewards', 'Chat'].map((label, index) => {
-          const path = `/${label.toLowerCase()}`;
-          const isActive = location.pathname === path;
-          
-          return (
-            <li key={index} ref={el => itemRefs.current[index] = el}>
-              <Link
-                to={path}
-                className={location.pathname === path ? 'active-link' : ''}
-                onClick={() => handleClick(index)}
-              >
-                {label}
-                {isActive && <span ref={lineRef} className='underline-line'></span>}
-              </Link>
-            </li>
-          );
-        })}
-        </ul>
-      </nav>
 
-    <div
-      className="navbar-footer"
-      onClick={() => {
-        if (location.pathname === "/Settings") {
-          navigate(previousPathRef.current || '/dashboard');
-        } else {
-          previousPathRef.current = location.pathname;
-          navigate('/Settings');
-          setActiveIndex(-1);
-        }
-      }}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-    >
-      {(location.pathname === "/Settings" && hovered) || 
-      (location.pathname !== "/Settings" && !hovered) ? (
-        <IoSettingsOutline size={45} color="#F3E7D3" />
-      ) : (
-        <IoSettings size={45} color="#F3E7D3" />
-      )}
-    </div>
+     const { offsetTop, offsetHeight, offsetLeft, offsetWidth } = link;
 
-    </div>
-  );
+
+     lineRef.current.style.top = `${offsetTop}px`; // align with top of link
+     lineRef.current.style.left = `0px`; // stay flush left
+     lineRef.current.style.width = `100%`; // full width of parent
+     lineRef.current.style.height = `3px`; // horizontal line like
+   }
+ }, [activeIndex]);
+
+
+ const handleClick = (index) => {
+   setActiveIndex(index);
+ };
+
+
+ // for logout button
+ const auth = getAuth(firebaseApp);
+ const [error, setError] = useState(null);
+
+
+ const handleLogout = async () => {
+   try {
+     await signOut(auth);
+     navigate("/login");
+   } catch (err) {
+     setError(err.message);
+   }
+ };
+
+
+ return (
+   <div className="navbar-container">
+     <div className="navbar-header">
+       <div
+         className="navbar-bg"
+         onClick={() => {
+           setActiveIndex(0);
+           navigate("/dashboard");
+         }}
+         style={{ cursor: "pointer" }}
+       >
+         <img src={logo} alt="Go Bear Logo" className="logo" />
+       </div>
+     </div>
+
+
+     <nav className="navbar-nav">
+       <ul>
+         {["Dashboard", "Teams", "Tasks", "Rewards", "Chat"].map(
+           (label, index) => {
+             const path = `/${label.toLowerCase()}`;
+             const isActive = location.pathname === path;
+
+
+             return (
+               <li key={index} ref={(el) => (itemRefs.current[index] = el)}>
+                 <Link
+                   to={path}
+                   className={location.pathname === path ? "active-link" : ""}
+                   onClick={() => handleClick(index)}
+                 >
+                   {label}
+                   {isActive && (
+                     <span ref={lineRef} className="underline-line"></span>
+                   )}
+                 </Link>
+               </li>
+             );
+           }
+         )}
+         <a
+           href="#logout"
+           className={location.pathname === "/logout" ? "active-link" : ""}
+           onClick={(e) => {
+             e.preventDefault();
+             handleLogout();
+           }}
+         >
+           Logout
+         </a>
+       </ul>
+     </nav>
+
+
+     <div
+       className="navbar-footer"
+       onClick={() => {
+         if (location.pathname === "/Settings") {
+           navigate(previousPathRef.current || "/dashboard");
+         } else {
+           previousPathRef.current = location.pathname;
+           navigate("/Settings");
+           setActiveIndex(-1);
+         }
+       }}
+       onMouseEnter={() => setHovered(true)}
+       onMouseLeave={() => setHovered(false)}
+     >
+       {(location.pathname === "/Settings" && hovered) ||
+       (location.pathname !== "/Settings" && !hovered) ? (
+         <IoSettingsOutline size={45} color="#F3E7D3" />
+       ) : (
+         <IoSettings size={45} color="#F3E7D3" />
+       )}
+     </div>
+   </div>
+ );
 }
 
+
 export default NavigationBar;
+
+
+
+
+
